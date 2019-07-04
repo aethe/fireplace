ac6354448fbcf97050e2b1cf724f8b6d4a3ada9a
Andrey Ufimtsev <aethe.dev@gmail.com>
2019-07-04 20:45:01 +0200

    Add sample code

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+// Create a logger
+let logger = Logger()
+
+// Add a console log
+logger.addLog(ConsoleLog())
+
+// Write a debug message
+// Debug messages don't appear in production
+logger.write("App is running on iPhone Xs.", level: .debug)
+
+// Write an info message
+// Info messages describe generic events
+logger.write("User allowed permission to access location.", level: .info)
+logger.write("If you don't specify level, info is assumed.")
+
+// Write a warning message
+// Warning messages describe non-bug problems
+logger.write("No internet connection.", level: .warning)
+
+// Write an error message
+// Error messages describe bugs
+logger.write("Failed to find image in app's bundle.", level: .error)
