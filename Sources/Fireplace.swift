a73add6c4769ef4d9c2a7b28ed8908f180ed5879
Andrey Ufimtsev <aethe.dev@gmail.com>
2019-09-05 21:08:08 +0200

    Trim empty lines

--- Sources/Fireplace.swift
+++ Sources/Fireplace.swift
@@ -16,11 +16,11 @@ public protocol Log: AnyObject {
 public final class ConsoleLog: Log {
     private let queue = DispatchQueue(label: "fireplace-console", qos: .utility)
     private let formatter: Formatter
-    
+
     public init(formatter: Formatter = PrettyFormatter()) {
         self.formatter = formatter
     }
-    
+
     public func write(_ message: Message) {
         queue.sync {
             print(formatter.string(from: message))
@@ -30,7 +30,7 @@ public final class ConsoleLog: Log {
 
 public final class FileLog: Log {
     private static let systemLog = OSLog(subsystem: "io.github.aethe.fireplace", category: "file-logging")
-    
+
     public static var defaultDirectoryURL: URL? {
         return FileManager
             .default
@@ -38,11 +38,11 @@ public final class FileLog: Log {
             .first?
             .appendingPathComponent("fireplace")
     }
-    
+
     public let url: URL
     private let fileHandle: FileHandle
     private let formatter: Formatter
-    
+
     public init?(url: URL, formatter: Formatter = PrettyFormatter()) {
         if !FileManager.default.fileExists(atPath: url.path) {
             guard FileManager.default.createFile(atPath: url.path, contents: nil) else {
@@ -50,39 +50,39 @@ public final class FileLog: Log {
                 return nil
             }
         }
-        
+
         guard let fileHandle = try? FileHandle(forWritingTo: url) else {
             os_log("Could not open a file for writing at %{public}@.", log: FileLog.systemLog, type: .error, url.path)
             return nil
         }
-        
+
         self.url = url
         self.fileHandle = fileHandle
         self.formatter = formatter
     }
-    
+
     public convenience init?(fileName: String, formatter: Formatter = PrettyFormatter()) {
         guard let directoryURL = FileLog.defaultDirectoryURL else {
             os_log("Could not get the default directory.", log: FileLog.systemLog, type: .error)
             return nil
         }
-        
+
         guard let _ = try? FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: true) else {
             os_log("Could not create a directory at %{public}@.", log: FileLog.systemLog, type: .error, directoryURL.path)
             return nil
         }
-        
+
         let fileURL = directoryURL.appendingPathComponent(fileName)
         self.init(url: fileURL, formatter: formatter)
     }
-    
+
     public convenience init?(formatter: Formatter = PrettyFormatter()) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd-HH-mm-ssZ"
         let fileName = "\(dateFormatter.string(from: Date())).txt"
         self.init(fileName: fileName, formatter: formatter)
     }
-    
+
     public func write(_ message: Message) {
         guard let data = "\(formatter.string(from: message))\n".data(using: .utf8) else { return }
         fileHandle.seekToEndOfFile()
@@ -104,7 +104,7 @@ public struct Message {
     public let timestamp: Date
     public let file: String
     public let line: Int
-    
+
     public init(text: String, level: Level, tags: [String], timestamp: Date = Date(), file: String = #file, line: Int = #line) {
         self.text = text
         self.level = level
@@ -121,11 +121,11 @@ public protocol Formatter {
 
 public struct PrettyFormatter: Formatter {
     private let dateFormatter = DateFormatter()
-    
+
     public init() {
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZZZ"
     }
-    
+
     public func string(from message: Message) -> String {
         let attributes = [
             levelComponent(from: message),
@@ -133,14 +133,14 @@ public struct PrettyFormatter: Formatter {
             locationComponent(from: message),
             tagComponent(from: message)
         ]
-        
+
         let attributeString = attributes
             .compactMap { $0 }
             .joined(separator: " ")
-        
+
         return [attributeString, message.text].joined(separator: ": ")
     }
-    
+
     private func levelComponent(from message: Message) -> String {
         switch message.level {
         case .debug: return "🚧"
@@ -149,15 +149,15 @@ public struct PrettyFormatter: Formatter {
         case .error: return "⛔"
         }
     }
-    
+
     private func timestampComponent(from message: Message) -> String {
         return "\(dateFormatter.string(from: message.timestamp))"
     }
-    
+
     private func locationComponent(from message: Message) -> String {
         return "@\(NSString(string: NSString(string: message.file).lastPathComponent).deletingPathExtension):\(message.line)"
     }
-    
+
     private func tagComponent(from message: Message) -> String? {
         return message.tags.isEmpty ? nil : message.tags.map { "#\($0)" }.joined(separator: " ")
     }
@@ -167,7 +167,7 @@ public enum Filter<T: Hashable> {
     case include(Set<T>)
     case exclude(Set<T>)
     case all
-    
+
     public func test(_ element: T) -> Bool {
         switch self {
         case .include(let set): return set.contains(element)
@@ -175,7 +175,7 @@ public enum Filter<T: Hashable> {
         case .all: return true
         }
     }
-    
+
     public func test(_ elements: [T]) -> Bool {
         switch self {
         case .include(let set): return elements.contains { set.contains($0) }
@@ -187,7 +187,7 @@ public enum Filter<T: Hashable> {
 
 public struct Obscured: CustomStringConvertible {
     public let description: String
-    
+
     public init(_ value: Any) {
         #if DEBUG
         description = "\(value)"
@@ -204,7 +204,7 @@ public struct Obscured: CustomStringConvertible {
 public final class Logger {
     private let queue = DispatchQueue(label: "fireplace-logger", qos: .utility)
     private var logs = [(log: Log, levels: Filter<Level>, tags: Filter<String>)]()
-    
+
     public func write(_ message: Message) {
         #if !DEBUG
         guard message.level != .debug else { return }
@@ -217,27 +217,27 @@ public final class Logger {
                 .forEach { $0.write(message) }
         }
     }
-    
+
     public func write(_ text: String, level: Level = .info, tags: [String] = [], file: String = #file, line: Int = #line) {
         write(Message(text: text, level: level, tags: tags, file: file, line: line))
     }
-    
+
     public func write(_ text: String, level: Level = .info, tags: String..., file: String = #file, line: Int = #line) {
         write(text, level: level, tags: tags, file: file, line: line)
     }
-    
+
     public func addLog(_ log: Log, levels: Filter<Level> = .all, tags: Filter<String> = .all) {
         queue.sync {
             logs.append((log, levels, tags))
         }
     }
-    
+
     public func removeLog(_ log: Log) {
         queue.sync {
             logs.removeAll { $0.log === log }
         }
     }
-    
+
     public func removeAllLogs() {
         queue.sync {
             logs.removeAll()
