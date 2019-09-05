02ff3342c592f7746d1d98a4488e0a9f47e15f95
Andrey Ufimtsev <aethe.dev@gmail.com>
2019-09-05 21:08:28 +0200

    Add documentation for Logger

--- Sources/Fireplace.swift
+++ Sources/Fireplace.swift
@@ -201,10 +201,17 @@ public struct Obscured: CustomStringConvertible {
     }
 }
 
+/// A logger for writing messages to logs.
 public final class Logger {
+    /// The queue all logging operations are performed on.
     private let queue = DispatchQueue(label: "fireplace-logger", qos: .utility)
+
+    /// An array of associated logs.
     private var logs = [(log: Log, levels: Filter<Level>, tags: Filter<String>)]()
 
+    /// Writes a new message to the associated logs.
+    ///
+    /// - Parameter message: The message to write.
     public func write(_ message: Message) {
         #if !DEBUG
         guard message.level != .debug else { return }
@@ -218,26 +225,52 @@ public final class Logger {
         }
     }
 
+    /// Writes a new message to the associated logs.
+    ///
+    /// - Parameters:
+    ///   - text: The text of the message.
+    ///   - level: The level of the message.
+    ///   - tags: The tags of the message.
+    ///   - file: The file where the write function was called from.
+    ///   - line: The line where the write function was called from.
     public func write(_ text: String, level: Level = .info, tags: [String] = [], file: String = #file, line: Int = #line) {
         write(Message(text: text, level: level, tags: tags, file: file, line: line))
     }
 
+    /// Writes a new message to the associated logs.
+    ///
+    /// - Parameters:
+    ///   - text: The text of the message.
+    ///   - level: The level of the message.
+    ///   - tags: The tags of the message.
+    ///   - file: The file where the write function was called from.
+    ///   - line: The line where the write function was called from.
     public func write(_ text: String, level: Level = .info, tags: String..., file: String = #file, line: Int = #line) {
         write(text, level: level, tags: tags, file: file, line: line)
     }
 
+    /// Adds a new log to the logger.
+    ///
+    /// - Parameters:
+    ///   - log: The log to add.
+    ///   - levels: The levels to filter messages by before writing to the log.
+    ///   - tags: The tags to filter the messages by before writing to the log.
     public func addLog(_ log: Log, levels: Filter<Level> = .all, tags: Filter<String> = .all) {
         queue.sync {
             logs.append((log, levels, tags))
         }
     }
 
+    /// Removes a specific log from the logger.
+    ///
+    /// - Parameter log: The log to remove.
     public func removeLog(_ log: Log) {
         queue.sync {
             logs.removeAll { $0.log === log }
         }
     }
 
+    /// Removes all logs from the logger.
     public func removeAllLogs() {
         queue.sync {
             logs.removeAll()
