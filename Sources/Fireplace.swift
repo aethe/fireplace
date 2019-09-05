8067c72f4a3804948fca8f2ccf112b54aa3fa13a
Andrey Ufimtsev <aethe.dev@gmail.com>
2019-09-05 21:17:24 +0200

    Add documentation for ConsoleLog

--- Sources/Fireplace.swift
+++ Sources/Fireplace.swift
@@ -13,14 +13,24 @@ public protocol Log: AnyObject {
     func write(_ message: Message)
 }
 
+/// A log written to the console.
 public final class ConsoleLog: Log {
+    /// The queue on which printing to the console is performed.
     private let queue = DispatchQueue(label: "fireplace-console", qos: .utility)
+
+    /// The formatter used to format messages.
     private let formatter: Formatter
 
+    /// Creates a new console log.
+    ///
+    /// - Parameter formatter: The formatter used to format messages.
     public init(formatter: Formatter = PrettyFormatter()) {
         self.formatter = formatter
     }
 
+    /// Prints a message to the console.
+    ///
+    /// - Parameter message: The message to print.
     public func write(_ message: Message) {
         queue.sync {
             print(formatter.string(from: message))
