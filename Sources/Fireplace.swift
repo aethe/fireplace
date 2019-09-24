9f06fac75bed958faaa1826c668d251e09c5c0de
Andrey Ufimtsev <aethe.dev@gmail.com>
2019-09-24 20:11:07 +0200

    Add documentation for PrettyFormatter

--- Sources/Fireplace.swift
+++ Sources/Fireplace.swift
@@ -179,13 +179,18 @@ public protocol Formatter {
     func string(from message: Message) -> String
 }
 
+/// A formatter that converts log messages into elegantly organised strings.
 public struct PrettyFormatter: Formatter {
+    /// The date formatter to format message timestamps.
     private let dateFormatter = DateFormatter()
 
+    /// Creates a new default formatter.
     public init() {
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZZZ"
     }
 
+    /// Converts a log message to a string which includes the level, the timestamp, the location in the file, the tags, and the text of the message.
+    /// - Parameter message: The message to format.
     public func string(from message: Message) -> String {
         let attributes = [
             levelComponent(from: message),
@@ -201,6 +206,8 @@ public struct PrettyFormatter: Formatter {
         return [attributeString, message.text].joined(separator: ": ")
     }
 
+    /// Returns a unicode emoji representing the level of a log message.
+    /// - Parameter message: The message being formatted.
     private func levelComponent(from message: Message) -> String {
         switch message.level {
         case .debug: return "🚧"
@@ -210,14 +217,20 @@ public struct PrettyFormatter: Formatter {
         }
     }
 
+    /// Formats the timestamp of a log message.
+    /// - Parameter message: The message being formatted.
     private func timestampComponent(from message: Message) -> String {
         return "\(dateFormatter.string(from: message.timestamp))"
     }
 
+    /// Formats the file location of a log message.
+    /// - Parameter message: The message being formatted.
     private func locationComponent(from message: Message) -> String {
         return "@\(NSString(string: NSString(string: message.file).lastPathComponent).deletingPathExtension):\(message.line)"
     }
 
+    /// Formats the tags of a log message.
+    /// - Parameter message: The message being formatted.
     private func tagComponent(from message: Message) -> String? {
         return message.tags.isEmpty ? nil : message.tags.map { "#\($0)" }.joined(separator: " ")
     }
