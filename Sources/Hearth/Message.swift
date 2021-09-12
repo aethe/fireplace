b2986f5a5faa07ef3485e0af44773676317832a2
Andrey Ufimcev <aethe.dev@gmail.com>
2021-09-12 18:09:25 +0100

    Update documentation

--- Sources/Hearth/Message.swift
+++ Sources/Hearth/Message.swift
@@ -18,22 +18,22 @@ public struct Message {
     /// The tags of the message.
     public let tags: [Tag]
 
-    /// The timestamp when the message has been recorded.
+    /// The timestamp when the message was recorded.
     public let timestamp: Date
 
-    /// The name of the file where the message has been recorded.
+    /// The name of the file where the message was recorded.
     public let file: String
 
-    /// The line in the file where the message has been recorded.
+    /// The line in the file where the message was recorded.
     public let line: Int
 
     /// Creates a new log message.
     /// - Parameter text: The text of the message.
     /// - Parameter level: The level of the message.
     /// - Parameter tags: The tags of the message.
-    /// - Parameter timestamp: The timestamp when the message has been recorded. Defaults to the current timestamp.
-    /// - Parameter file: The name of the file where the message has been recorded. Defaults to the current file.
-    /// - Parameter line: The line in the file where the message has been recorded. Defaults to the current line.
+    /// - Parameter timestamp: The timestamp when the message was recorded. Defaults to the current timestamp.
+    /// - Parameter file: The name of the file where the message was recorded. Defaults to the current file.
+    /// - Parameter line: The line in the file where the message was recorded. Defaults to the current line.
     public init(text: String, level: Level, tags: [Tag], timestamp: Date = Date(), file: String = #file, line: Int = #line) {
         self.text = text
         self.level = level
